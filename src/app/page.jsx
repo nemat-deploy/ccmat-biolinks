9fe69e3cb6429172f5cd6418aad0f0904bdf97de
Ricardo Melo <ricardomelo.rt@gmail.com>
2024-08-27 18:32:20 -0300

    update

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,24 +5,28 @@ import LinkButton from "./components/LinkButton";
 export default function Home() {
   return (
     <main className="main">
+
       <div className="logo">
         <Image src="/images/logo-laef-ufdpar.svg" 
         alt="Logo" 
         width={130} 
         height={130} />
       </div>
+
       <div className="description">
         Liga Acadêmica de Educaçao Financeira
       </div>
-      <LinkButton 
-        href="https://sites.google.com/view/laefufdpar/in%C3%ADcio"
-        >Site institucional</LinkButton>
-      <LinkButton 
-        href="https://docs.google.com/document/d/1R0x3nBFmszOBFnVB7z3bokxXoWJ1dCPINlm9BUhPO_4/edit?usp=sharing"
-        >Edital 2024 <br/> Novos Membros
+
+      <LinkButton href="https://sites.google.com/view/laefufdpar/in%C3%ADcio">
+        Site institucional
       </LinkButton>
+
       <LinkButton href="https://forms.gle/xhscLPgXjvoLwkhv7" target="_blank">
-        Formulário de Inscrição
+        Formulário de Inscrição <br/> 2024
+      </LinkButton>
+
+      <LinkButton href="https://docs.google.com/document/d/1R0x3nBFmszOBFnVB7z3bokxXoWJ1dCPINlm9BUhPO_4/edit?usp=sharing">
+        Edital 2024 <br/> Novos Membros
       </LinkButton>
       
     </main>
