0b8b6546c97f09bfd0ddd505192de6151b2d199a
Ricardo Melo <ricardomelo.rt@gmail.com>
2024-09-24 22:24:25 -0300

    update

--- src/app/fluxograma/page.jsx
+++ src/app/fluxograma/page.jsx
@@ -1,21 +1,27 @@
 'use client'
 
 import React from 'react'
+import './Page.module.css'
 
 function Fluxograma(){
     const handleImageOpen = ()=> {
         window.open("/images/fluxograma-matematica-ufdpar-1.jpg")
     }
+    const handleImagePdf = ()=> {
+        window.open("/images/fluxograma-matematica-ufdpar.pdf")
+    }
 
     return (
-        <main>
-            <button onClick={handleImageOpen}>
-                ABRIR COMO IMAGEM JPG
-            </button>
-            <button>
-                BAIXAR EM PDF
-            </button>
-        </main>
+        <div className='mainFluxograma'>
+            <div className="options">
+                <button className='btImage' onClick={handleImageOpen}>
+                    ABRIR COMO IMAGEM JPG
+                </button>
+                <button className='btImage' onClick={handleImagePdf}>
+                    BAIXAR EM PDF
+                </button>
+            </div>
+        </div>
     )
 }
 
