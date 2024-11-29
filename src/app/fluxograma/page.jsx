ba28f93933ca884d9ea90225f2a75c088fffcede
Ricardo Melo <ricardomelo.rt@gmail.com>
2024-11-28 23:38:31 -0300

    update

--- src/app/fluxograma/page.jsx
+++ src/app/fluxograma/page.jsx
@@ -5,9 +5,12 @@ import styles from './Page.module.css'
 
 function Fluxograma(){
     const handleImageOpen = ()=> {
-        window.open("/images/fluxograma-matematica-ufdpar-1.jpg")
+        window.open("/docs/pre-requisitos-2021.pdf")
     }
-    const handleImagePdf = ()=> {
+    const handleResumidoPdf = ()=> {
+        window.open("/docs/fluxograma-resumido.pdf")
+    }
+    const handleCompletoPdf = ()=> {
         window.open("/docs/fluxograma-matematica-ufdpar.pdf")
     }
 
@@ -18,11 +21,17 @@ function Fluxograma(){
             <div className="options">
                 <h3>Fluxograma do Curso de Matemática - UFDPar</h3>
                 <button className='btImage' onClick={handleImageOpen}>
-                    BAIXAR COMO IMAGEM
+                    Pre-requisitos
                 </button>
-                <button className='btImage' onClick={handleImagePdf}>
-                    BAIXAR EM PDF
+
+                <button className='btImage' onClick={handleResumidoPdf}>
+                    Fluxograma Resumido
                 </button>
+
+                <button className='btImage' onClick={handleCompletoPdf}>
+                    Fluxograma Resumido
+                </button>
+
             </div>
         </div>
     )
