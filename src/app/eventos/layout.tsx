50d571c0bf5e8b901b4fcd322938c903796c9da6
Ricardo Melo <nemat.ufdpar@gmail.com>
2025-10-05 19:19:04 -0300

    Setting link /evntos/ on WhatsApp

--- src/app/eventos/layout.tsx
+++ src/app/eventos/layout.tsx
@@ -1,10 +1,29 @@
-// src/app/eventos/layout.tsx
-"use client";
-
 import './layout.css';
 import { ReactNode } from "react";
 import Image from 'next/image';
 import FooterEventos from '../components/FooterEventos';
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'App EVENTOS',
+  description: 'by NEMAT/UFDPar',
+  openGraph: {
+    title: 'App EVENTOS',
+    description: 'by NEMAT/UFDPar',
+    url: 'https://matematica-ufdpar.vercep.app/eventos',
+    siteName: 'App EVENTOS',
+    images: [
+      {
+        url: 'https://matematica-ufdpar.vercel.app/images/logo-app-eventos-og.png',
+        width: 1200,
+        height: 630,
+        alt: 'Logo App Eventos',
+      },
+    ],
+    locale: 'pt_BR',
+    type: 'website',
+  },
+};
 
 export default function EventosLayout({ children }: { children: ReactNode }) {
   return (
